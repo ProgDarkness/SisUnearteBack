f765ec6abbff02f259591929695f5bd6e954300b
ProgDarkness <gabrielmarcano141@gmail.com>
2023-08-21 09:51:05 -0400

    Revert "ajustes a funciones para crear materias y carreras"
    This reverts commit 9cc9a1568e62cfbc10e531aa93343b15ae4b4f7e.

--- src/GraphQL/Resolvers/Carrera.js
+++ src/GraphQL/Resolvers/Carrera.js
@@ -34,23 +34,82 @@ export default {
                                             r001t_docente_materia AS dm, t003t_personal AS p, m045t_estatus_carrera AS ec
                                             WHERE cm.id_trayecto = t.id_trayecto AND cm.id_materia = m.id_materia AND
                                             dm.id_personal = p.id_personal AND dm.id_materia = m.id_materia 
-                                            AND ec.id_estatus_carrera = c.id_estatus_carrera AND c.id_carrera = $1;`,
-          [carrera]
-        )
+                                            AND ec.id_estatus_carrera = c.id_estatus_carrera AND c.id_carrera = $1;`, [carrera]);
+                
+                if (detallecarreras) {
+                    return {status: 200, message: 'Carreras encontrado', type: "success", response: detallecarreras}
+                } else {
+                    return {status: 202, message: 'Carreras no encontrado', type: "error"}
+                }
+            } catch (e) {
+                return {status: 500, message: `Error: ${e.message}`, type: "error"}
+            }
+        },
+        crearCarrera: async (_, {input}) => {
+            const {carrera, codigo, nombre, credito, tipo, hora} = input
+            
+            try {
+                const idMateria = await dbp.oneOrNone(
+                    `INSERT INTO public.m005t_materias(
+                      co_materia, nb_materia, nu_credito, id_tp_materia, hr_semanal, id_estatus_materia)
+                      VALUES ($1, $2, $3, $4, $5, $6) RETURNING id_materia;`, [codigo, nombre, credito, tipo, hora, 4])
+                
+                await dbp.none(
+                    `INSERT INTO public.r002t_carrera_materia(
+                        id_carrera, id_materia, visible, hora_semanal)
+                        VALUES ($1, $2, $3, $4, $5);`, [carrera, idMateria, true, hora]
+                )
 
-        if (detallecarreras) {
-          return {
-            status: 200,
-            message: 'Carreras encontrado',
-            type: 'success',
-            response: detallecarreras
-          }
-        } else {
-          return {
-            status: 202,
-            message: 'Carreras no encontrado',
-            type: 'error'
-          }
+                return {status: 200, message: 'Materia registrada exitosamente', type: "success"}
+            } catch (e) {
+                return {status: 500, message: `Error: ${e.message}`, type: "error"}
+            }
+        },
+        actualizarCarrera: async (_, {input}) => {
+            const {codigo, nombre, tipo, ciclo, titulo, idcarrera} = input
+            
+            try {     
+                await dbp.none(
+                    `UPDATE public.m006t_carreras
+                    SET co_carrera = $1, nb_carrera = $2, id_tp_carrera = $3, id_ciclo = $4, titulo_otorgado = $5
+                    WHERE id_carrera = $6;`, [codigo, nombre, tipo, ciclo, titulo, idcarrera])
+
+                return {status: 200, message: 'Carrera actualizada exitosamente', type: "success"}
+            } catch (e) {
+                return {status: 500, message: `Error: ${e.message}`, type: "error"}
+            }
+        },
+        eliminarCarrera: async (_, {input}) => {
+            const {idcarrera} = input
+            
+            try {  
+                const carreraperiodo = await dbp.manyOrNone(`SELECT * FROM r006t_periodo_carrera pc WHERE pc.id_carrera = $1;`, [idcarrera]);
+
+                const carreramateria = await dbp.manyOrNone(`SELECT * FROM r002t_carrera_materia cm WHERE cm.id_carrera = $1;`, [idcarrera]);
+
+                const carrerapostulacion = await dbp.manyOrNone(`SELECT * FROM t013t_postulacion p WHERE p.id_carrera = $1;`, [idcarrera]);
+
+                if (carreraperiodo || carreramateria || carrerapostulacion) {
+                    return {status: 202, message: 'Carrera no puede ser eliminada asociada a otros datos', type: "success"}
+                } else {
+                    await dbp.none(`DELETE FROM public.m006t_carreras WHERE id_carrera = $1;`, [idcarrera])
+
+                    return {status: 200, message: 'Carrera eliminada exitosamente', type: "success"} 
+                }
+            } catch (e) {
+                return {status: 500, message: `Error: ${e.message}`, type: "error"}
+            }
+        },
+        actualizarEstatusCarrera: async (_, {input}) => {
+            const {estatus, idcarrera} = input
+            
+            try {     
+                await dbp.none(`UPDATE public.m006t_carreras SET id_estatus_carrera = $1 WHERE id_carrera = $2;`, [estatus, idcarrera])
+
+                return {status: 200, message: 'Estatus de la carrera actualizada exitosamente', type: "success"}
+            } catch (e) {
+                return {status: 500, message: `Error: ${e.message}`, type: "error"}
+            }
         }
       } catch (e) {
         return { status: 500, message: `Error: ${e.message}`, type: 'error' }
